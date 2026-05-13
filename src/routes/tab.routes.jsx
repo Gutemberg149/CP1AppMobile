e16eae3e50da4f4d6b3390b92b9966adc86e66cb
guhstr <labsfiap@fiap.com.br>
2026-05-13 09:34:30 -0300

    Foram feitas as pastas screens, routes, componentes e context. Instalação do Botton Tab, e já feita a rota da página de Home. App.js refactorado.

---
+++ src/routes/tab.routes.jsx
@@ -0,0 +1,18 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "../screens/HomeScreen"
+import {Feather} from "@expo/vector-icons"
+
+const Tab = createBottomTabNavigator();
+
+export default function TabRoutes(){
+    return(
+        <Tab.Navigator screenOptions={{headerShown:false}}>
+            <Tab.Screen 
+                name="home"
+                component={HomeScreen}
+                options={{tabBarIcon:()=><Feather name="home" color="green" size={30}/>,tabBarLabel:"Inicio",}}
+
+            />
+        </Tab.Navigator>
+    )
+}
